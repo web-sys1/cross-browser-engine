37022d3c0080c74949cd045e38ea61055f3e4580
Web Source (SRC) <web.system.management@gmail.com>
2021-09-08 17:47:40 +0300

    Update screenshot.js

--- api/screenshot.js
+++ api/screenshot.js
@@ -96,7 +96,7 @@ module.exports = async (req, res) => {
   } catch (e) {
     res.statusCode = 500;
     res.json({
-      body: "Sorry, Something went wrong!",
+      body: "Sorry, Something went wrong! Probably, the problem is with the system. Please try again!",
     });
   }
 };
